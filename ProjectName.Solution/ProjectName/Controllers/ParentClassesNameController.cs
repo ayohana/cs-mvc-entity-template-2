4c0e56e056eb1a9fab548e80de20c4143a53e3ba
Adela Darmansyah <52761827+ayohana@users.noreply.github.com>
2020-03-23 10:15:37 -0700

    Add directive to ParentClassesNameController

--- ProjectName.Solution/ProjectName/Controllers/ParentClassesNameController.cs
+++ ProjectName.Solution/ProjectName/Controllers/ParentClassesNameController.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ProjectName.Models;
 using System.Collections.Generic;
 using System.Linq;
