388a1c4ae71cf04b74ec8c6885da2079b190824e
Adela Darmansyah <52761827+ayohana@users.noreply.github.com>
2020-03-27 08:49:14 -0700

    Update ParentClassName.cs

--- ProjectName.Solution/ProjectName/Models/ParentClassName.cs
+++ ProjectName.Solution/ProjectName/Models/ParentClassName.cs
@@ -5,7 +5,7 @@ namespace ProjectName.Models
   public class ParentClassName
   {
     public int ParentClassNameId { get; set; }
-    public ICollection<ParentChildClassName> ParentChildClassesName { get; set; }
+    public ICollection<ParentChildClassName> ClassesName { get; set; }
     public ParentClassName()
     {
       this.ClassesName = new HashSet<ParentChildClassName>();
